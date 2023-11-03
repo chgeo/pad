4db45f43fcd9e3d656c2da99109c2403ebf8821f
Christian Georgi <chgeo@users.noreply.github.com>
2023-11-03 13:55:27 +0530

    Create flights-service.js

---
+++ types/flights-service.js
@@ -0,0 +1,25 @@
+const cds = require('@sap/cds')
+
+class FlightsService extends cds.ApplicationService {
+  async init() {
+
+    //  without cds-typer
+    // const { Flights } = cds.entities
+    // service.before('CREATE', Flights, ({ data }) => {
+    //      /* data is of type any */
+    //      data[0]
+    // })
+
+    // ✨ with cds-typer
+    // const { Flights } = require('#cds-models/FlightsService')
+    // this.before('CREATE', Flights, ({ data }) => { 
+    //     /* data is of type Flights */
+    //     data[0].flightDate
+    // })
+    
+
+    return super.init()
+  }
+}
+
+module.exports = FlightsService
